0e866b4ca9d49389d224cba335b1d7998412e169
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-02-01 15:12:58 +0700

    revert: change font awesome link to react-icons

--- src/api/socialMediaData.js
+++ src/api/socialMediaData.js
@@ -1,32 +1,34 @@
+import { FaWordpress, FaTwitter, FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
+
 const socialMediaData = [
   {
     code: 'blog',
     href: 'https://indrakusuma.web.id',
-    icon: 'fa fa-wordpress',
+    icon: FaWordpress,
     title: 'Read my story on my own Blog :)',
   },
   {
     code: 'twitter',
     href: 'https://twitter.com/id_indrakusuma',
-    icon: 'fa fa-twitter',
+    icon: FaTwitter,
     title: 'Follow Indra on Twitter!',
   },
   {
     code: 'facebook',
     href: 'https://fb.com/id.indrakusuma',
-    icon: 'fa fa-facebook',
+    icon: FaFacebook,
     title: 'Follow Indra on Facebook!',
   },
   {
     code: 'github',
     href: 'https://github.com/idindrakusuma',
-    icon: 'fa fa-github',
+    icon: FaGithub,
     title: 'Follow Indra on Github!',
   },
   {
     code: 'linkedin',
     href: 'https://linkedin.com/in/idindrakusuma',
-    icon: 'fa fa-linkedin',
+    icon: FaLinkedin,
     title: 'Follow Indra on Linkedin!',
   },
 ];
