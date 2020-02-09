152ea67b581312711d6e9ad78d7b03a9a5081a04
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-02-09 09:47:10 +0700

    feat: finish the project section | #2

--- src/api/worksData.js
+++ src/api/worksData.js
@@ -5,7 +5,7 @@ export const awards = [
     isAward: true,
     isPortofolio: false,
     desc: `Tokopedia Dev-Camp is a 5 days intensive training and hackathon program for final year or post-graduate students with tech-related background. <br /><br />
-    In this event, Tokopedia offer the opportunity to be involved in intensive tech training program, an exposure of how Tokopedia operates as well as hands on experience on to develop product during Tokopedia Dev-Camp Hackathon. Additionally, get huge exposure to network with the management and a golden ticket to work as a full time engineer at Tokopedia.`,
+    I was one of 39 participants chosen from 1800 registrants. With this competition, I get the innternship opportunity and now as full-time engineer in Tokopedia.`,
     date: 'Aug 2018',
   },
   {
@@ -14,9 +14,9 @@ export const awards = [
     isAward: true,
     isPortofolio: false,
     date: 'March 2018',
-    desc: `Dalam perlombaan ini, pihak penyelenggara memeberikan studi kasus mengenai Danau Toba. Kemudian kami menganalisa permasalahan yang terjadi dan memberikan solusi dengan penerapatan teknologi informasi (IT) untuk meningkatkan pengunjung ke Danau Toba. <br /><br />
+    desc: `In this competition, the organizer give us the problem about Toba lake. So, we the participants need to give solutionn how to increase touriest in Toba lake using Information Technology. <br /> <br />
 
-    Solusi yang kami tawarkan adalah AyoToba - Sebuah aplikasi yang berfungsi untuk menguatkan branding pariwisata Danau toba dilengkapi dengan fitur menemukan tourguide , fotografer dan berbagai informasi seperti penginapan, kuliner, transportasi maupun cerita rakyat seputar Danau Toba.`,
+    We give all in one app solution and we called it 'AyoToba'. AyoToba is super app to inncrease touriest in Danau Toba lake. This app give the user information about hotels, culinary, tranportation, also photographer.`,
   },
   {
     title: '3rd place in Startup Business Competition',
@@ -70,5 +70,65 @@ export const awards = [
     date: 'May 2014',
     desc:
       'We have many majors in our schools and me got the best graduated in Natioan Exam and School Exam in SMK MUhammadiyah 03 Weleri ✨',
+  },
+];
+
+export const projects = [
+  {
+    title: 'Tokopedia Lite Cart & Checkout Page',
+    location: 'Tokopedia, Jakarta',
+    isAward: false,
+    isPortofolio: true,
+    date: 'Nov 2018 - Now',
+    desc: 'I was develop several features especially in Cart & Checkout page in Tokopedia.',
+    link: 'https://m.tokopedia.com/cart',
+  },
+  {
+    title: 'The Panic Button',
+    location: 'University Dian Nuswantoro, Semarang',
+    isAward: false,
+    isPortofolio: true,
+    date: 'Nob 2016',
+    desc:
+      'We built the alarm to sent location and message via SMS using Raspberry Pi with custom interval with just 1 click button which is integrated in Motorcycle to avoid the emergency condition',
+    link: '',
+  },
+  {
+    title: 'FLS App dan Website Management',
+    location: 'Nusantara Muda, Semarang',
+    isAward: false,
+    isPortofolio: true,
+    date: 'November - Aug 2017',
+    desc:
+      'We built several things in Future Leader Summit like Landing page website, blog, register page, the intools to manage registrants, and we also built the in app to guide participants to select breakfast also get the current information in agenda of event',
+    link: 'https://github.com/creativefls',
+  },
+  {
+    title: "IT Management System UKM BAI Matholi'ul Anwar",
+    location: 'Semarang',
+    isAward: false,
+    isPortofolio: true,
+    date: 'Oct 2016 – Jan 2017',
+    desc:
+      'BAI is a student club in Dian Nuswantoro University. I was built IT Management System to help BAI to manage members, to handle the website and to manage the finance',
+    link: 'http://bai.dinus.ac.id/',
+  },
+  {
+    title: 'Landing Page & Registrants page Future Leader Summit 2016',
+    location: 'Nusantara Muda, 2016',
+    isAward: false,
+    isPortofolio: true,
+    date: 'Apr 2016 – Oct 2016',
+    desc: 'This project is the first project in FLS. Actually, I just carry on the previously project',
+    link: '',
+  },
+  {
+    title: 'Villa Charity Web Blog Information',
+    location: 'Semarang',
+    isAward: false,
+    isPortofolio: true,
+    date: 'Aug 2015',
+    desc: 'I was built the landing page with blogger engine to make online information about Villa Charity Bandungan',
+    link: 'http://villacharity-bandungan.blogspot.com/',
   },
 ];
--- src/pages/works.js
+++ src/pages/works.js
@@ -10,21 +10,28 @@ import { Timeline, Event } from '@components/Timeline';
 
 import ImgPatners from '@assets/patners.jpg';
 import TestimonialsData from '@/api/testimonial';
-import { awards } from '@/api/worksData';
+import { awards, projects } from '@/api/worksData';
 import { Typography, styFlexCenter } from '@/styles';
 
-const TRESHOLD = 4;
+const TRESHOLD = 3;
 
 function WorksPage() {
   const [isShowAllAward, setIsShowAllAward] = useState(false);
+  const [isShowAllProject, setIsShowAllProject] = useState(false);
 
-  const isShowButton = useMemo(() => {
+  const isShowButtonAward = useMemo(() => {
     if (!isShowAllAward && awards.length > TRESHOLD) return true;
 
     return false;
   }, [isShowAllAward]);
 
-  const renderButton = () => {
+  const isShowButtonProject = useMemo(() => {
+    if (!isShowAllProject && projects.length > TRESHOLD) return true;
+
+    return false;
+  }, [isShowAllProject]);
+
+  const renderButtonAwards = () => {
     if (isShowAllAward) {
       return (
         <Button small onClick={() => setIsShowAllAward(false)}>
@@ -33,10 +40,30 @@ function WorksPage() {
       );
     }
 
-    if (isShowButton) {
+    if (isShowButtonAward) {
       return (
         <Button small onClick={() => setIsShowAllAward(true)}>
-          <span css={styFlexCenter}>Show more 🤙</span>
+          <span css={styFlexCenter}>Show more awards 🤙</span>
+        </Button>
+      );
+    }
+
+    return null;
+  };
+
+  const renderButtonProjects = () => {
+    if (isShowAllProject) {
+      return (
+        <Button small onClick={() => setIsShowAllProject(false)}>
+          <span css={styFlexCenter}>Show less 👋</span>
+        </Button>
+      );
+    }
+
+    if (isShowButtonProject) {
+      return (
+        <Button small onClick={() => setIsShowAllProject(true)}>
+          <span css={styFlexCenter}>Show more projects 👨‍💻</span>
         </Button>
       );
     }
@@ -53,6 +80,34 @@ function WorksPage() {
 
     awards.some((item, index) => {
       if (!isShowAllAward) {
+        if (index < TRESHOLD) {
+          showItems.push(
+            <Event key={index} interval={item.date} title={item.title} subTitle={item.location} showIcon>
+              <div dangerouslySetInnerHTML={{ __html: item.desc }} />
+            </Event>,
+          );
+        } else {
+          return true;
+        }
+      } else {
+        showItems.push(
+          <Event key={index} interval={item.date} title={item.title} subTitle={item.location} showIcon>
+            <div dangerouslySetInnerHTML={{ __html: item.desc }} />
+          </Event>,
+        );
+      }
+
+      return false;
+    });
+
+    return showItems;
+  };
+
+  const renderProjects = () => {
+    const showItems = [];
+
+    projects.some((item, index) => {
+      if (!isShowAllProject) {
         if (index < TRESHOLD) {
           showItems.push(
             <Event key={index} interval={item.date} title={item.title} subTitle={item.location} showIcon>
@@ -94,7 +149,12 @@ function WorksPage() {
       <FunFacts projects={30} customers={27} hours={4500} awards={7} />
       <Card title="Awards">
         <Timeline>{renderAwards()}</Timeline>
-        <div style={{ margin: '0 auto', display: 'table' }}>{renderButton()}</div>
+        <div style={{ margin: '0 auto', display: 'table' }}>{renderButtonAwards()}</div>
+      </Card>
+      <Divider />
+      <Card title="Projects">
+        <Timeline>{renderProjects()}</Timeline>
+        <div style={{ margin: '0 auto', display: 'table' }}>{renderButtonProjects()}</div>
       </Card>
       <Card>
         {renderTitle('How do they said about me? 🤔', '10px 0 30px 0')}
