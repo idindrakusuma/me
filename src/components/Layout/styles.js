81015fb6fb4f691645fc1043e1aa87040e4be576
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-02-15 11:14:04 +0700

    test: add test case for skills

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -13,19 +13,6 @@ export const WrapperBody = styled.div`
 `;
 
 export const Wrapper = styled.div`
-  * {
-    font-family: 'Roboto Slab', serif;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-family: 'Montserrat', sans-serif;
-  }
-
   img {
     max-width: 100%;
     height: auto;
