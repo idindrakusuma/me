f17e24a5dc9bade7faa99090e1e9f3b8dfecf6b1
Indra Kusuma <indrakusuma.udinus@gmail.com>
2020-02-21 06:28:19 +0700

    chore: add e2e using cypress | #31

---
+++ cypress/e2e/accessibility.spec.js
@@ -0,0 +1,12 @@
+describe('Accessbility test for homepage', () => {
+  beforeEach(() => {
+    cy.visit('/').injectAxe();
+  });
+
+  it('user can open `/about` when click button `btnWhoIndra` in homepage', () => {
+    cy.get('[data-testid="btnWhoIndra"]').click();
+    cy.location().should(location => {
+      expect(location.pathname).to.eq('/about');
+    });
+  });
+});
